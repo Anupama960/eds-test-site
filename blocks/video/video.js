6beefbe2d94ad3fdf22565494bb9a55355ca9fc0
Anupama <Anupamauthaman06@gmail.com>
2025-08-12 01:02:06 +0530

    updated video.js

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -6,36 +6,16 @@ export default function decorate(block) {
     return;
   }
 
-  let videoHtml = '';
+  const match = assetPath.match(/vimeo\.com\/(?.video\/)?(\d+)/);
+  const videoId = match ? match[1] : null;
 
-  if (assetPath.includes('vimeo.com')) {
-    const match = assetPath.match(/vimeo\.com\/(?:video\/)?(\d+)/);
-    const videoId = match ? match[1] : null;
-    if (videoId) {
-      videoHtml = `
-        <iframe
+  const videoHtml = <iframe
 src="https://player.vimeo.com/video/${videoId}"
           width="640" height="360"
           frameborder="0"
           allow="autoplay; fullscreen; picture-in-picture"
           allowfullscreen>
-        </iframe>`;
-    } else {
-      videoHtml = '<p>Invalid Vimeo URL</p>';
-    }
-  } else if (assetPath.endsWith('git statua.mp4')) {
-    videoHtml = `
-      <video controls width="640" height="360">
-        <source src="${assetPath}" type="video/mp4">
-        Your browser does not support the video tag.
-      </video>`;
-  } else {
-    videoHtml = `
-      <video controls width="640" height="360">
-        <source src="${assetPath}">
-        Your browser does not support the video tag.
-      </video>`;
-  }
+        </iframe>;
 
   block.innerHTML = videoHtml;
 }
