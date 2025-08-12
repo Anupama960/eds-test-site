bd80f755c8510bd332965c2613f8800fdddfc630
Anupama <Anupamauthaman06@gmail.com>
2025-08-12 13:28:16 +0530

    updated video component

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,20 +1,31 @@
 export default function decorate(block) {
-  const { videoPath } = block.dataset;
-
+  const videoPath = block.children[0]?.textContent?.trim();
+  const caption = block.children[1]?.textContent?.trim();
+ 
   if (!videoPath) {
     block.textContent = 'No video';
     return;
   }
-
+ 
+ 
   const videoEl = document.createElement('video');
-  videoEl.setAttribute('controls', true);
-  videoEl.setAttribute('preload', 'metadata');
+  videoEl.controls = true;
+  videoEl.preload = 'metadata';
+ 
 
   const source = document.createElement('source');
   source.src = videoPath;
-  source.type = 'video/mp4';
-
+  source.type = videoPath.endsWith('.webm') ? 'video/webm' : 'video/mp4';
+ 
   videoEl.appendChild(source);
+ 
   block.innerHTML = '';
   block.appendChild(videoEl);
+ 
+  if (caption) {
+    const captionEl = document.createElement('p');
+    captionEl.textContent = caption;
+    block.appendChild(captionEl);
+  }
 }
+ 
