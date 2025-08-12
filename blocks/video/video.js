61be8bcfbd4a41da72872b2441d431f578c37711
Anupama <Anupamauthaman06@gmail.com>
2025-08-12 08:18:02 +0530

    updated video.js

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,21 +1,24 @@
 export default function decorate(block) {
-  const assetPath = (block.dataset.videoasset || '').trim();
+  const assetPath = (block.dataset.videoasset || "").trim();
 
   if (!assetPath) {
-    block.innerHTML = '<p>No video selected</p>';
+    block.innerHTML = "<p>No video</p>";
     return;
   }
 
   const match = assetPath.match(/vimeo\.com\/(?.video\/)?(\d+)/);
   const videoId = match ? match[1] : null;
 
-  const videoHtml = <iframe
-src="https://player.vimeo.com/video/${videoId}"
-          width="640" height="360"
-          frameborder="0"
-          allow="autoplay; fullscreen; picture-in-picture"
-          allowfullscreen>
-        </iframe>;
+  const videoHtml = (
+    <iframe
+      src="https://player.vimeo.com/video/${videoId}"
+      width="640"
+      height="360"
+      frameborder="0"
+      allow="autoplay; fullscreen; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+  );
 
   block.innerHTML = videoHtml;
 }
