b37b7cb9269a0866cbc383806c1e19484b45718f
Anupama <Anupamauthaman06@gmail.com>
2025-08-12 10:53:24 +0530

    added video.css

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,24 +1,17 @@
 export default function decorate(block) {
   const assetPath = (block.dataset.videoasset || "").trim();
 
-  if (!assetPath) {
+  if (!videoPath) {
     block.innerHTML = "<p>No video</p>";
     return;
   }
 
-  const match = assetPath.match(/vimeo\.com\/(?.video\/)?(\d+)/);
-  const videoId = match ? match[1] : null;
-
-  const videoHtml = (
-    <iframe
-      src="https://player.vimeo.com/video/${videoId}"
-      width="640"
-      height="360"
-      frameborder="0"
-      allow="autoplay; fullscreen; picture-in-picture"
-      allowfullscreen
-    ></iframe>
-  );
+  const videoHtml = 
+    <video controls width="640" height="360">
+        <source src="${videoPath}" type="video/mp4" />
+        doesn't support video tag.
+    </video>
+     
 
   block.innerHTML = videoHtml;
 }
