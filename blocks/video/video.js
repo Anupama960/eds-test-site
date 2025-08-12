968e07e6577c85bb77e98514d412b9ae0387793b
Anupama <Anupamauthaman06@gmail.com>
2025-08-12 12:34:46 +0530

    added video.css

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,17 +1,18 @@
 export default function decorate(block) {
-  const assetPath = (block.dataset.videoasset || "").trim();
+  let videoPath = (block.dataset.videoasset || "").trim();
 
   if (!videoPath) {
     block.innerHTML = "<p>No video</p>";
     return;
   }
 
-  const videoHtml = 
-    <video controls width="640" height="360">
+  if (videoPath.startsWith('/content/dam')) {
+    videoPath = '${window.location.origin}${videoPath}';
+  }
+
+
+  block.innerHTML = <video controls width="640" height="360">
         <source src="${videoPath}" type="video/mp4" />
         doesn't support video tag.
-    </video>
-     
-
-  block.innerHTML = videoHtml;
+    </video>;
 }
