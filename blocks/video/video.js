565dab6c062b9c2d228a74bff534de2fd86474f5
Anupama <Anupamauthaman06@gmail.com>
2025-08-21 01:45:50 +0530

    updated video component

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,12 +1,12 @@
 export default function decorate(block) {
   const videoUrl = block.textContent.trim(); // Assume block has Vimeo URL
-  block.textContent = "";
+  block.textContent = '';
 
-  const iframe = document.createElement("iframe");
-  iframe.src = videoUrl.replace("vimeo.com/", "player.vimeo.com/video/");
-  iframe.width = "640";
-  iframe.height = "360";
-  iframe.allow = "autoplay; fullscreen; picture-in-picture";
+  const iframe = document.createElement('iframe');
+  iframe.src = videoUrl.replace('vimeo.com/', 'player.vimeo.com/video/');
+  iframe.width = '640';
+  iframe.height = '360';
+  iframe.allow = 'autoplay; fullscreen; picture-in-picture';
   iframe.allowFullscreen = true;
 
   block.appendChild(iframe);
@@ -14,7 +14,5 @@ export default function decorate(block) {
   // Initialize Vimeo player
   const player = new Vimeo.Player(iframe);
 
-  player.on("play", function () {
-    console.log("Video is playing");
-  });
+  player.on('play', function () {});
 }
